536f6e0a1ab5652b0e049cd07cf012f315bc776b
zooyaho <jiwoo1268@naver.com>
2022-06-17 14:28:51 +0900

    drag애니메이션 적용

--- src/App.tsx
+++ src/App.tsx
@@ -13,53 +13,28 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  background-color: rgba(255, 255, 255, 0.2);
+  background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const Circle = styled(motion.div)`
-  background-color: white;
-  height: 70px;
-  width: 70px;
-  place-self: center;
-  border-radius: 35px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
-
 const boxVariants = {
-  start: { opsity: 0, scale: 0.5 },
-  end: { 
-    opsity: 1, 
-    scale: 1, 
-    transition: { 
-      type: "spring", 
-      duration: 0.5, 
-      bounce: 0.5,
-      delayChildren: 0.5,
-      staggerChildren: 0.2 // 자식요소들에게 순서대로 증감하는 delay적용
-    } 
-  },
+  hover: {scale: 1.5, rotateZ:90},
+  click: {scale:1, borderRadius: "100px"}
 }
 
-const circleVariants = {
-  start: { opacity: 0, y:10 },
-  end: { opacity: 1, y:0 },
-}
 
 function App() {
   return (
     <Wrapper>
-      <Box variants={boxVariants} initial="start" animate="end">
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-      </Box>
+      <Box
+        drag
+        whileDrag={{backgroundColor:"rgb(255, 234, 167)"}}
+        variants={boxVariants}
+        whileHover="hover"
+        whileTap="click"
+      />
     </Wrapper>
   );
 }
-
 export default App;
