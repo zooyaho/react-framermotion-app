d0bf5c8bbda50fd7704cfe471194e199f5054a9a
zooyaho <jiwoo1268@naver.com>
2022-06-18 18:01:40 +0900

    첫번째 박스를 클릭하면 overlay위 정가운데로 이동하는 animation 적용

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import { motion, AnimatePresence } from "framer-motion";
 import { useState } from 'react';
 
-const Wrapper = styled(motion.div)`
+const Wrapper = styled.div`
   height: 100vh;
   width: 100vw;
   display: flex;
@@ -11,39 +11,51 @@ const Wrapper = styled(motion.div)`
   align-items: center;
 `;
 
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  width: 70vw;
+  gap: 10px;
+  div:first-child,
+  div:last-child {
+    grid-column: span 2;
+  }
+`;
+
 const Box = styled(motion.div)`
-  width: 400px;
-  height: 400px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
+  height: 200px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const Overlay = styled(motion.div)`
+  width: 100%;
+  height: 100%;
+  position: absolute;
   display: flex;
   justify-content: center;
   align-items: center;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
+  `;
 
-const Circle = styled(motion.div)`
-  background-color: #00a5ff;
-  height: 100px;
-  width: 100px;
-  border-radius: 50px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
 
 function App() {
   const [clicked, setClicked] = useState(false);
-  const toggleClicked = ()=>{
-    setClicked((prev)=> !prev);
+  const toggleClicked = () => {
+    setClicked((prev) => !prev);
   }
 
   return (
     <Wrapper onClick={toggleClicked}>
-      <Box>
-        {clicked ? <Circle layoutId='circle' style={{borderRadius:0}}/> : null}
-      </Box>
-      <Box>
-        {!clicked ? <Circle layoutId='circle' style={{borderRadius:50}}/> : null}
-      </Box>
+      <Grid>
+        <Box layoutId='box1'/>
+        <Box />
+        <Box />
+        <Box />
+      </Grid>
+      <AnimatePresence>
+        {clicked ? <Overlay initial={{ backgroundColor: "rgba(0, 0, 0, 0)" }} animate={{ backgroundColor: "rgba(0, 0, 0, .5)"}} exit={{ backgroundColor: "rgba(0, 0, 0, 0)"}}><Box layoutId='box1' style={{ width: 400, height: 200 }} /></Overlay> : null}
+      </AnimatePresence>
     </Wrapper>
   );
 }
