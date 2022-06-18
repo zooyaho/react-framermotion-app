204dc7d8586a6927691fcdd601175ce6db189bcf
zooyaho <jiwoo1268@naver.com>
2022-06-18 16:47:26 +0900

    next,prev버튼 실행되는 슬라이드 구현

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ const Wrapper = styled(motion.div)`
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
 `;
 
 const Box = styled(motion.div)`
@@ -17,33 +18,53 @@ const Box = styled(motion.div)`
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   position: absolute;
-  top: 100px;
+  top: 150px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 28px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const boxVariants = {
-  initial: { opacity: 0, scale: 0 },
-  visible: { opacity: 1, scale: 1, rotateZ: 360 },
-  leaving: { opacity: 0, scale: 0, y: 50 }
-}
 
 function App() {
-  const [showing, setShwoing] = useState(false);
-  const toggleHandler = () => { setShwoing(prev => !prev) }
+  const [visible, setVisible] = useState(1);
+  const [back, setBack] = useState(false); // 슬라이드가 뒤로가는지 확인하는 state
+  const nextPlease = () => {
+    setBack(false);
+    setVisible(prev => prev === 10 ? 10 : prev + 1)
+  }
+  const prevPlease = () => {
+    setBack(true);
+    setVisible(prev => prev === 1 ? 1 : prev - 1)
+  }
 
+  const boxVariants = {
+    // 인수인 isBack은 Box의 custom={back}으로 받은 값임.
+    entry: (isBack: boolean) => { return { opacity: 0, scale: 0, x: isBack?-500: 500 } },
+    visible: { opacity: 1, scale: 1, x: 0, transition: { duration: .3 } },
+    exit: (isBack: boolean) => { return { opacity: 0, scale: 0, x: isBack?500: -500, transition: { duration: .3 } } },
+  }
   return (
     <Wrapper>
-      <button onClick={toggleHandler}>Click me</button>
-      <AnimatePresence>
-        {showing ? (
-          <Box
-            variants={boxVariants}
-            initial="initial"
-            animate="visible"
-            exit="leaving"
-          />
-        ) : null}
+      {/* 
+        AnimatePresence에도 custom 작성,
+        exitBeforeEnter : 전 요소의 animation이 완전히 끝난 후 다음 animation이 작동 함. ex) <AnimatePresence exitBeforeEnter custom={back}>
+      */}
+      <AnimatePresence custom={back}>
+        <Box
+          custom={back} // back에 따라 boxVariants이 달라짐.
+          key={visible} // React js의 children은 각 고유의 key가 필요하기 때문에 key값이 변하면 다른 요소로 변함. 
+          variants={boxVariants}
+          initial="entry"
+          animate="visible"
+          exit="exit"
+        >
+          {visible}
+        </Box>
       </AnimatePresence>
+      <button onClick={nextPlease} style={{ fontSize: "30px" }}>next</button>
+      <button onClick={prevPlease} style={{ fontSize: "30px" }}>prev</button>
     </Wrapper>
   );
 }
